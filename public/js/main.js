ba0ce6aec4e37c0585ccacebc7851342a01b8969
Aswin-k-ashok <aswinkashok0@gmail.com>
2022-10-25 22:46:24 +0530

    application finished

--- public/js/main.js
+++ public/js/main.js
@@ -1,41 +1,62 @@
-const chatForm = document.getElementById("chat-form");
-const chatMessages = document.querySelector(".chat-messages");
-const socket = io();
+const chatForm = document.getElementById('chat-form')
+const chatMessages = document.querySelector('.chat-messages')
+const roomName = document.getElementById('room-name')
+const userList= document.getElementById('users')
+const socket = io()
 
 // GET USER NAME AND ROOM FROM PARMS
 const { username, room } = Qs.parse(location.search, {
-    ignoreQueryPrefix:true
+  ignoreQueryPrefix: true,
 })
-console.log(username,room)
 
-socket.on("message", (message) => {
+//JOIN ROOM
+socket.emit('joinRoom', { username, room })
+
+// GET ROOM AND USERS
+socket.on('roomUsers',({room,users})=>{
+  outputRoomName(room)
+  outputUsers(users)
+})
+
+socket.on('message', (message) => {
   // message from event emiter in server.js
-  console.log(message);
-  outputMessage(message);
- 
+  console.log(message)
+  outputMessage(message)
+
   //SCROLL DOWN
-  chatMessages.scrollTop = chatMessages.scrollHeight;
-});
+  chatMessages.scrollTop = chatMessages.scrollHeight
+})
 
 //MESSAGE SUBMIT
-chatForm.addEventListener("submit", (e) => {
-  e.preventDefault();
+chatForm.addEventListener('submit', (e) => {
+  e.preventDefault()
 
   // GET MESSAGE TEXT
-  const msg = e.target.elements.msg.value;
+  const msg = e.target.elements.msg.value
 
   // EMIT MESSAGE TO SERVER
-  socket.emit("chatMessage", msg);
+  socket.emit('chatMessage', msg)
 
   //CLEAR INPUT
-  e.target.elements.msg.value = "";
-  e.target.elements.msg.focus();
-});
+  e.target.elements.msg.value = ''
+  e.target.elements.msg.focus()
+})
 
 //OUTPUT MESSAGE TO DOM
 function outputMessage(message) {
-  const div = document.createElement("div");
-  div.classList.add("message");
-  div.innerHTML = `<p class="meta">${message.username}<span>${message.time}</span></p> <p class-"text> ${message.text} </p>`;
-  document.querySelector(".chat-messages").appendChild(div);
+  const div = document.createElement('div')
+  div.classList.add('message')
+  div.innerHTML = `<p class="meta">${message.username}<span>${message.time}</span></p> <p class-"text> ${message.text} </p>`
+  document.querySelector('.chat-messages').appendChild(div)
+}
+
+// add room name to dom
+function outputRoomName(room){
+  roomName.innerText = room;
+
+}
+
+// add users to dom
+function outputUsers(users){
+  userList.innerHTML= `${users.map(user=>`<li>${user.username}</li>`).join('')}`
 }
--- server.js
+++ server.js
@@ -1,41 +1,73 @@
-const express = require('express');
+const express = require('express')
 const path = require('path')
 const http = require('http')
 const socketio = require('socket.io')
 const formatMessage = require('./utils/messages')
+const {
+  userJoin,
+  getCurrentUser,
+  userLeave,
+  getRoomUsers,
+} = require('./utils/users')
 
-const app = express();
-const server = http.createServer(app); // transfering the express app to the server
-const io = socketio(server)  // transfering the server functionality to the socket io . this is basically an event emitter
+const app = express()
+const server = http.createServer(app) // transfering the express app to the server
+const io = socketio(server) // transfering the server functionality to the socket io . this is basically an event emitter
 
 // static folder
-app.use(express.static(path.join(__dirname,'public')))  // setting up the static files AKA frontend
+app.use(express.static(path.join(__dirname, 'public'))) // setting up the static files AKA frontend
 
 //admin variable
 const admin = 'sochat admin'
 
 // run when client connet
 
-io.on("connection", socket => {
+io.on('connection', (socket) => {
+  socket.on('joinRoom', ({ username, room }) => {
+    const user = userJoin(socket.id, username, room)
+
+    socket.join(user.room)
 
     // WELCOME CURRENT USER
-    socket.emit('message', formatMessage(admin,"welcome to sochat"))  // a new event is emited from this event emiter and , this event will be catched in public > js > main.js
-    
+    socket.emit('message', formatMessage(admin, 'welcome to sochat')) // a new event is emited from this event emiter and , this event will be catched in public > js > main.js
+    console.log(user.username)
     //BOADCAST MESSAGE WHEN A USER CONNECTS
-    socket.broadcast.emit('message',formatMessage(admin, 'a user has joined the chat'));
-
-    // RUNS WHEN CLIENT DISCONNECTS
-    socket.on('disconnect', () => {
-        io.emit('message',formatMessage(admin,'a user has left the chat'));
+    socket.broadcast
+      .to(user.room)
+      .emit(
+        'message',
+        formatMessage(admin, `${user.username} has joined the chat`)
+      )
+    //SEND USERS AND ROOM INFO
+    io.to(user.room).emit('roomUsers', {
+      room: user.room,
+      users: getRoomUsers(user.room),
     })
+  })
 
-    // LISTEN FOR CHAT MESSAGE
-    socket.on('chatMessage', (msg) => {
-        io.emit("message",formatMessage('user', msg))
-    })
+  // LISTEN FOR CHAT MESSAGE
+  socket.on('chatMessage', (msg) => {
+    const user = getCurrentUser(socket.id)
+    io.to(user.room).emit('message', formatMessage(user.username, msg))
+  })
+  // RUNS WHEN CLIENT DISCONNECTS
+  socket.on('disconnect', () => {
+    const user = userLeave(socket.id)
+
+    if (user) {
+      io.to(user.room).emit(
+        'message',
+        formatMessage(admin, `${user.username} has left the chat`)
+      )
+      //SEND USERS AND ROOM INFO
+      io.to(user.room).emit('roomUsers', {
+        room: user.room,
+        users: getRoomUsers(user.room),
+      })
+    }
+  })
 })
-
 
 const PORT = 8080 || process.env.port
 
-server.listen(PORT,()=> console.log("server running on",PORT));
+server.listen(PORT, () => console.log('server running on', PORT))
